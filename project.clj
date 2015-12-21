25c14f3fd80720b51c12889ae3bd8fdcef370a42
Jacob Morzinski <jmorzins@mit.edu>
2015-12-21 01:52:28 -0500

    Move the map of words to a Jar resource.

--- project.clj
+++ project.clj
@@ -6,4 +6,12 @@
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot diceware.core
   :target-path "target/%s"
+;;; Jar Output
+  ;; Leave the contents of :source-paths out of jars (for AOT projects).
+  :omit-source true
+  ;; Files with names matching any of these patterns will be excluded from jars.
+  ;;:jar-exclusions [#"(?:^|/).svn/"]
+  ;; Same thing, but for uberjars.
+  ;;:uberjar-exclusions [#"META-INF/DUMMY.SF"]
+
   :profiles {:uberjar {:aot :all}})
