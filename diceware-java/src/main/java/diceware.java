26fd40b83da568a9797510f922cb6a7c446c2f61
Jacob Morzinski <jmorzins@mit.edu>
2016-01-03 20:20:55 -0500

    Add skeleton of Java version.
    Uses IntelliJ IDEA and Gradle.

---
+++ diceware-java/src/main/java/diceware.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Jacob on 1/3/2016.
+ */
+
+public class diceware {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        return;
+    }
+}
