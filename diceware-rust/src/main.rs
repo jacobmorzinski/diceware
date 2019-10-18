f3e756a908fd7cff26c04925d657370f3e5fd124
Jacob Morzinski <morzinski@gmail.com>
2019-10-17 22:01:50 -0400

    Convert from clap to structopt

--- diceware-rust/src/main.rs
+++ diceware-rust/src/main.rs
@@ -1,49 +1,43 @@
-#[macro_use]
-extern crate clap;
-
-use clap::{App, AppSettings, Arg};
 use std::iter;
+use structopt::{clap::AppSettings, StructOpt};
 
-use diceware;
+#[derive(Debug, StructOpt)]
+#[structopt(about, setting(AppSettings::UnifiedHelpMessage))]
+struct Opt {
+    #[structopt(
+        short = "j",
+        long = "join",
+        default_value = "\n",
+        hide_default_value = true, // it doesn't print properly
+        help = "Join words using separator [default: \\n]"
+    )]
+    separator: String,
+
+    #[structopt(
+        required = false,
+        default_value = "4",
+        help = "Sets the number of diceware words to select"
+    )]
+    number: usize,
+
+    #[structopt(
+        long,
+        hidden = true,
+        required = false,
+        possible_values = &["stream", "roll", "simple"],
+        default_value = "stream",
+        help = "Internal"
+    )]
+    method: String,
+}
 
 pub type Result<T> = std::result::Result<T, diceware::Error>;
 
 fn main() -> Result<()> {
-    let matches = App::new(crate_name!())
-        .version(crate_version!())
-        .about(crate_description!())
-        .setting(AppSettings::DisableVersion) // no --version flag
-        .setting(AppSettings::UnifiedHelpMessage)
-        .arg(
-            Arg::with_name("SEPARATOR")
-                .short("j")
-                .long("join")
-                .takes_value(true)
-                .default_value("\n")
-                .hide_default_value(true) // it doesn't print properly
-                .help("Join words using separator [default: \\n]"),
-        )
-        .arg(
-            Arg::with_name("METHOD")
-                .short("m")
-                .long("method")
-                .hidden(true)
-                .required(false)
-                .possible_values(&["stream", "roll", "simple"])
-                .default_value("stream")
-                .help("Internal"),
-        )
-        .arg(
-            Arg::with_name("NUMBER")
-                .required(false)
-                .default_value("4")
-                .help("Sets the number of diceware words to select"),
-        )
-        .get_matches();
-
-    let separator = matches.value_of("SEPARATOR").unwrap();
-    let number = value_t!(matches.value_of("NUMBER"), usize).unwrap_or_else(|e| e.exit());
-    let method = matches.value_of("METHOD").unwrap();
+    let opt = Opt::from_args();
+    let separator: &str = &opt.separator;
+    let number: usize = opt.number;
+    let method: &str = &opt.method;
 
     if method == "stream" {
         let word_stream = iter::repeat_with(|| diceware::get_word());
