09f4742def1ceefe6f7152ab84d34a04f80b9108
Jacob Morzinski <morzinski@gmail.com>
2020-05-25 02:28:23 -0400

    Clojure options with clojure.tools.cli

--- diceware-clojure/src/diceware/core.clj
+++ diceware-clojure/src/diceware/core.clj
@@ -2,6 +2,9 @@
   (:refer-clojure :exclude [rand rand-int rand-nth])
   (:require [clojure.java.io :as io]
             [clojure.edn :as edn]
+            [clojure.tools.cli :as cli]
+            [clojure.string :as string]
+            [clojure.pprint]
             [diceware.secure-rand :as securerandom])
   (:gen-class))
 
@@ -28,12 +31,46 @@
       slurp
       edn/read-string))
 
+(defn join-character [s]
+  (-> s
+      str
+      first))
+
+(defn parse-int [n]
+  (-> n
+      edn/read-string
+      int))
+
+(def cli-options
+  [["-j" "--join CHARACTER" "Join words with CHARACTER (default \\n)"
+    :default \newline
+    :parse-fn join-character]
+   ["-n" "--number NUMBER" "Number of words to generate"
+    :default 5
+    :parse-fn parse-int]
+   ["-h" "--help"]])
+
+(defn usage [options-summary]
+  (->> ["Usage: clj -m diceware.main [options]"
+        ""
+        "Options:"
+        options-summary]
+       (string/join \newline)))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (string/join \newline errors)))
+
 (defn -main
   "Print some random die rolls"
   [& args]
-  (def n (try
-           (Integer/parseInt (first args))
-           (catch Exception e 5)))
-  (dotimes [_ n]
-    (let [roll (five-rolls)]
-      (println (roll diceware-map)))))
+  (let [{:keys [options arguments errors summary]}
+        (cli/parse-opts args cli-options)]
+    (cond
+      (:help options) (println (usage summary))
+      (some? errors) (println (error-msg errors))
+      :else
+      (let [{:keys [join number]} options]
+        (dotimes [_ number]
+          (let [roll (five-rolls)]
+            (println (roll diceware-map))))))))
